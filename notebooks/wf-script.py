f329b3ad5e6928093d28c6c881b36849fa4db3f9
Isaac Overcast <isaac.overcast@gmail.com>
2025-02-19 14:16:00 -0500

    add file logging

--- notebooks/wf-script.py
+++ notebooks/wf-script.py
@@ -6,12 +6,13 @@ import random
 
 class Population:
     import numpy as np
-    def __init__(self, N=10, f=0.2, with_np=False):
+    def __init__(self, N=10, f=0.2, outfile=None, with_np=False):
         self.N = N
         self.f = f
         self.with_np = with_np
         self.steps = 0
         self.state = "segregating"
+        self.outfile = outfile
 
         derived_count = round(N*f)
         self.pop = [0] * (N - derived_count) + [1] * derived_count
@@ -23,6 +24,8 @@ class Population:
         return f"Population(N={self.N}, f={self.f})"
 
     def step(self, ngens=1):
+        if self.outfile:
+            out = open(self.outfile, 'a')
         for i in range(ngens):
             # Test fixation/loss
             derived = sum(self.pop)
@@ -37,7 +40,11 @@ class Population:
                 self.pop = np.random.choice(self.pop, size=self.N)
             else:
                 self.pop = random.choices(self.pop, k=self.N)
+            if self.outfile:
+                out.write("".join([str(x) for x in self.pop]) + "\n")
             self.steps += 1
+        if self.outfile:
+            out.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -47,6 +54,7 @@ if __name__ == "__main__":
                         type=int, default=1000)
     parser.add_argument("-f", help="Initial derived allele frequency", 
                         type=float, default=0.2)
+    parser.add_argument("-o", help="Outfile", dest="outfile")
     parser.add_argument("--withnp",
                          help="Use numpy rather than lists",
                          action="store_true")
@@ -58,6 +66,6 @@ if __name__ == "__main__":
     if args.verbose:
         print(args)
 
-    p = Population(N=args.N, f=args.f, with_np=args.withnp)
+    p = Population(N=args.N, f=args.f, outfile=args.outfile, with_np=args.withnp)
     p.step(ngens=args.ngens)
     print(f"{p.state} after generations: {p.steps}; freq(a): {sum(p.pop)/p.N}")
